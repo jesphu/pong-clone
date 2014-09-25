3ddbb8a5ac5f3c406f12772129e6558173ad70d4
Jessica Phuong <jessicaphuong02@gmail.com>
2014-09-25 22:27:36 +0930

    Change style of the code and file name

--- pang.cpp
+++ pang.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv) { // A * token followed by another * token in this case is a pointer to a pointer. In this case, argv is a pointer to an array of char*. It is equivalent to (int argc, char* argv[]).
     Game::Start();
